fd1bc001bfb1169af7d1683558b51008a1aa1315
ivogonzaa <ivogonzaa@gmail.com>
2021-09-18 16:04:44 -0300

    Cambio modulo http por https

--- src/app.js
+++ src/app.js
@@ -1,23 +1,35 @@
-/*
-Creo que hay que cambiar el modulo "http" por el de "https" 
-para lo de la vulnerabilidad
-*/
-
-const http = require("http");
+const https = require("https");
+const fs = require("fs");
 
 const reader = require("./utils/page-reader")
 
-const server = http.createServer((req, res) => {
+const options = {
+    key: fs.readFileSync("./cert/key.pem"),
+    cert: fs.readFileSync("./cert/cert.pem"),
+    requestCert: true,
+    rejectUnauthorized: false
+}
+
+const server = https.createServer(options, (req, res) => {
+    //Css
     if (req.url.match("\.css$")) {
         const cssFileName = req.url.slice(1, -4);
         const css = reader.getCssFromFile(cssFileName);
+        res.writeHead(200, { 'Content-Type': 'text/css', 'Content-Length': css.length });
         res.end(css);
+        return;
     }
-    if (req.url === "/login") {
-        const html = reader.getHtmlFromFile("login");
+    //Urls
+    const htmlFileName = req.url.slice(1);
+    const html = reader.getHtmlFromFile(htmlFileName);
+    if (html && req.url.match("(\/\w*)+")) {
         res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': html.length });
         res.end(html);
+        return;
     }
+    //Error
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end("<h1>Page not found!</h1>");
 });
 
 server.listen(3000);
